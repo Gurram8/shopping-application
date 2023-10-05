e2433c7075a33f5efb07963b6e49f70db365a6d7
Gurram8 <bgurram8@gmail.com>
2023-10-05 13:05:16 -0500

    Commit #1

---
+++ product-service/src/main/java/com/shoppingapplication/productservice/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.shoppingapplication.productservice.repository;
+
+import com.shoppingapplication.productservice.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ProductRepository extends MongoRepository<Product, String> {
+
+}
